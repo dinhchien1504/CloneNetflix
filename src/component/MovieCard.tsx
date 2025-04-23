995fda718c83f6457ab484f92b065d97f01e04ee
minhlc1108 <minh.lcm.2003@gmail.com>
2025-04-24 01:02:27 +0700

    chrolo

--- src/component/MovieCard.tsx
+++ src/component/MovieCard.tsx
@@ -19,13 +19,10 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
     <div className="group bg-zinc-900 col-span h-[12vw] relative">
       <Image
         className="cursor-pointer object-cover transition duration-300 shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 w-full h-[12vw]"
-        
         src={movie.thumb_url.startsWith("https") ? movie.thumb_url : `https://phimimg.com/${movie.thumb_url}`}
         alt={`Movie Thumbnail - ${movie.thumb_url}`}
-
         width={300} height={450}
       />
-     
       <div
         className="opacity-0 absolute top-0 transition-all duration-300 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw]  group-hover:opacity-100 
 ease-in-out 
@@ -49,8 +46,7 @@ ease-in-out
               // onClick={() => openModal(slug)}
             >
               {/* <BsFillPlayFill size={30} > */}
-              <PlayButton episodeSlug={''} slug={movie.slug} shape={"circle"} content={<BsFillPlayFill className="hover:cursor-pointer" size={25} />} 
-      
+              <PlayButton epIndex={0} slug={movie.slug} shape={"circle"} content={<BsFillPlayFill className="hover:cursor-pointer" size={25} />} 
              />
             </div>
 
@@ -76,11 +72,6 @@ ease-in-out
           <p className="text-green-400 font-semibold mt-1">
             Year : <span className="text-white">{movie.year}</span>
           </p>
-
-
-          {/* <div className="flex flex-row mt-4 gap-2 items-center">
-            <p className='text-white text-[10px] lg:text-sm'>{genre}</p>
-          </div> */}
         </div>
       </div>
     </div>
